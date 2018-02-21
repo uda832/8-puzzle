11f664de8c7c312ce5da75bf41e11daffd089a30
O Yeruultsengel <oyeruultsengel@etelligentsolutions.com>
2018-02-21 12:57:27 -0500

    init

---
+++ 8puzzle.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, tiles,):
+    
+class EightTilesSolver:
+    def __init__(self, tiles):
+        self.start = start      # Note: represent each state 
+
+    '''
+        Prints the contents of the tiles
+    '''
+    def printNode(self, node):
+        print(node[0:2])       # Row 0
+        print(node[3:5])       # Row 0
+        print(node[6:8])       # Row 0
+               
+        
+        
+
+def main():
+    print("Hello World")
+
+    # Read input
+
+    # Call solver
+
+
+main()
